0a5b42ed16e8f48942f1c10ab00e8419d6213501
Lee Len Formento <leebatef@gmail.com>
2018-12-05 17:43:14 -0500

    another migration and seed modification

---
+++ data/migrations/20181205174149_notes_table_four.js
@@ -0,0 +1,25 @@
+
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('notes_table_four', function(tbl) {
+        tbl.increments();
+
+        tbl
+        .string('title', 150)
+        .notNullable();
+
+        tbl
+        .string('textBody', 1000)
+        .notNullable();
+
+        tbl
+        .timestamp('created_at') // default to timestamptz in PostgreSQL unless true
+        .defaultTo(knex.fn.now());
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropIfTableExists('notes_table_four')
+  
+};
--- data/seeds/01-notes_table.js
+++ data/seeds/01-notes_table.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('notes_table_three').truncate()
+  return knex('notes_table_four').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('notes_table_three').insert([
-        {note_title: 'title 1', textBody: 'content 1'},
-        {note_title: 'title 2', textBody: 'content 2'},
-        {note_title: 'title 3', textBody: 'content 3'}
+      return knex('notes_table_four').insert([
+        {title: 'title 1', textBody: 'content 1'},
+        {title: 'title 2', textBody: 'content 2'},
+        {title: 'title 3', textBody: 'content 3'}
       ]);
     });
 };
